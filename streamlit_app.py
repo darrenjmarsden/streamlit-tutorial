7adcd3d5bf83aa9b7490002afceaff28f8dbffb8
darrenjmarsden <darrenjmarsden@btinternet.com>
2024-11-01 20:52:07 +0000

    day10

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,13 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+st.write("Day 10")
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
